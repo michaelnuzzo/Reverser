0712bf831e152a9d2945f8353f7108c4017190e2
Nuzzo, Michael Louis <nuzzomic@fel.cvut.cz>
2020-11-04 17:34:08 -0500

    updated look and feel

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -13,22 +13,26 @@
 ReverserAudioProcessorEditor::ReverserAudioProcessorEditor (ReverserAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    addAndMakeVisible(timeKnob);
+    timeKnob.getLookAndFeel().setColour(juce::Slider::textBoxTextColourId, juce::Colours::black);
     timeKnob.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
     timeKnob.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 50, 20);
     timeKnob.onValueChange = [this] {audioProcessor.setUpdate();};
     timeAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.getParameters(),"TIME",timeKnob);
     timeLabel.setText("Window Length (ms)", juce::dontSendNotification);
     timeLabel.setJustificationType(juce::Justification::horizontallyCentred);
+    timeLabel.setColour(juce::Label::textColourId, juce::Colours::black);
     timeLabel.attachToComponent(&timeKnob, false);
+    addAndMakeVisible(timeKnob);
 
-    addAndMakeVisible(dryWetKnob);
+    dryWetKnob.getLookAndFeel().setColour(juce::Slider::textBoxTextColourId, juce::Colours::black);
     dryWetKnob.setSliderStyle(juce::Slider::RotaryVerticalDrag);
     dryWetKnob.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 20);
     dryWetAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.getParameters(),"DRYWET",dryWetKnob);
     dryWetLabel.setText("Dry/Wet (%)", juce::dontSendNotification);
     dryWetLabel.setJustificationType(juce::Justification::horizontallyCentred);
+    dryWetLabel.setColour(juce::Label::textColourId, juce::Colours::black);
     dryWetLabel.attachToComponent(&dryWetKnob, false);
+    addAndMakeVisible(dryWetKnob);
 
     setResizable(true, true);
     setResizeLimits(400, 300, 1600, 900);
@@ -42,10 +46,18 @@ ReverserAudioProcessorEditor::~ReverserAudioProcessorEditor()
 //==============================================================================
 void ReverserAudioProcessorEditor::paint (juce::Graphics& g)
 {
+
+
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-    g.setColour (juce::Colours::white);
+    g.fillAll (juce::Colours::aliceblue);
     g.setFont (15.0f);
+
+    juce::Image logo = juce::ImageCache::getFromMemory (BinaryData::logo_png, BinaryData::logo_pngSize);
+    logo = logo.rescaled(logo.getWidth()/2., logo.getHeight()/2.);
+    float xPos = getWidth()/2.-logo.getWidth()/2.;
+    float yPos = getHeight()/3.-logo.getHeight()/2.;
+
+    g.drawImageAt(logo, xPos, yPos);
 }
 
 void ReverserAudioProcessorEditor::resized()

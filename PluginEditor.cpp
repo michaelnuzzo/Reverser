b1c23b43ec3ac799b380bb3838f02030ce4b25e3
Nuzzo, Michael Louis <nuzzomic@fel.cvut.cz>
2020-11-04 15:05:09 -0500

    GUI updates

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -13,21 +13,26 @@
 ReverserAudioProcessorEditor::ReverserAudioProcessorEditor (ReverserAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-
     addAndMakeVisible(timeKnob);
     timeKnob.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
     timeKnob.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, false, 50, 20);
     timeKnob.onValueChange = [this] {audioProcessor.setUpdate();};
     timeAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.getParameters(),"TIME",timeKnob);
-    
-    
+    timeLabel.setText("Window Length (ms)", juce::dontSendNotification);
+    timeLabel.setJustificationType(juce::Justification::horizontallyCentred);
+    timeLabel.attachToComponent(&timeKnob, false);
 
     addAndMakeVisible(dryWetKnob);
     dryWetKnob.setSliderStyle(juce::Slider::RotaryVerticalDrag);
     dryWetKnob.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 20);
     dryWetAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.getParameters(),"DRYWET",dryWetKnob);
+    dryWetLabel.setText("Dry/Wet (%)", juce::dontSendNotification);
+    dryWetLabel.setJustificationType(juce::Justification::horizontallyCentred);
+    dryWetLabel.attachToComponent(&dryWetKnob, false);
 
-    setSize (400, 300);
+    setResizable(true, true);
+    setResizeLimits(400, 300, 1600, 900);
+    setSize (800, 600);
 }
 
 ReverserAudioProcessorEditor::~ReverserAudioProcessorEditor()
@@ -39,13 +44,17 @@ void ReverserAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
 }
 
 void ReverserAudioProcessorEditor::resized()
 {
-    timeKnob.setBounds(100, 200, 100, 100);
-    dryWetKnob.setBounds(300, 200, 100, 100);
+    float knobWidth = 150;
+    float xPos1 = getWidth()/3.-knobWidth/2.;
+    float xPos2 = 2*getWidth()/3.-knobWidth/2.;
+    float yPos = getHeight()-300;
+
+    timeKnob.setBounds(xPos1, yPos,knobWidth,knobWidth);
+    dryWetKnob.setBounds(xPos2, yPos,knobWidth,knobWidth);
 }
